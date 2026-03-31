afae99f12a2ba75e4b3bf38330d28e21cfb82166
maramnidal521-tech <maramnidal521@gmail.com>
2026-03-31 17:07:04 +0300

    updated

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,33 +1,39 @@
-import { notFound } from "next/navigation";
-import { posts } from "@/app/api/posts/route";
+import { notFound } from "next/navigation"
 
 async function getPost(id) {
-  const post = posts.find((elem) => elem.id === +id);
-  return post || null;
+    const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    cache: "no-store"
+    })
+
+    if (!res.ok) {
+        return null;
+    }
+
+    return res.json();
 }
 
 export async function generateMetadata({ params }) {
-  const { id } = await params;
-  const post = await getPost(id);
+    const { id } = await params;
+    const post = await getPost(id);
 
-  return {
-    title: post?.title || "Post",
-    description: post?.content || "Post details"
-  };
+    return {
+        title: post?.title || "Post",
+        description: post?.content || "Post details"
+    }
 }
 
 export default async function PostPage({ params }) {
-  const { id } = await params;
-  const post = await getPost(id);
+    const { id } = await params;
+    const post = await getPost(id);
 
-  if (!post) {
-    notFound();
-  }
+    if (!post) {
+        notFound();
+    }
 
-  return (
-    <div className="container">
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+        </div>
+    )
 }
